1e7a4b766d6c4e9b1c6ed056982fa9df2c65cc2d
Hyunggeun447 <bopi447@naver.com>
2022-10-28 23:42:29 +0900

    test: 익명게시판 추가 및 싱글톤 테스트

--- src/test/java/mission/mission/domain/team/service/TeamServiceTest.java
+++ src/test/java/mission/mission/domain/team/service/TeamServiceTest.java
@@ -3,11 +3,14 @@ package mission.mission.domain.team.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import mission.mission.domain.board.entity.Board;
+import mission.mission.domain.board.repository.BoardRepository;
 import mission.mission.domain.team.dto.request.CreateTeamRequest;
 import mission.mission.domain.team.dto.request.UpdateTeamRequest;
 import mission.mission.domain.team.entity.Team;
 import mission.mission.domain.team.repository.TeamRepository;
 import mission.mission.domain.team.value.Gender;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -25,6 +28,11 @@ class TeamServiceTest {
 
   @Autowired
   TeamRepository teamRepository;
+
+  @AfterEach
+  void clean() {
+    teamRepository.deleteAll();
+  }
 
   @Nested
   @DisplayName("save")
@@ -126,6 +134,92 @@ class TeamServiceTest {
           () -> teamRepository.findById(teamId).orElseThrow(RuntimeException::new));
 
     }
+
+  }
+
+  @Nested
+  @DisplayName("addAnonymousBoard")
+  class addAnonymousBoard {
+
+    Long teamId;
+
+    @BeforeEach
+    void setup() {
+      String teamName = "엑소";
+      Gender gender = Gender.MALE;
+      CreateTeamRequest request = CreateTeamRequest.builder()
+          .name(teamName)
+          .gender(gender)
+          .build();
+
+      teamId = teamService.save(request);
+    }
+
+    @Test
+    @DisplayName("성공")
+    public void S() throws Exception {
+
+      //when
+      teamService.addAnonymousBoard(teamId);
+
+      //then
+      Team team = teamRepository.findById(teamId).orElseThrow(RuntimeException::new);
+      assertThat(team.getBoardList().size()).isEqualTo(1);
+      assertThat(team.getExistAnonymous()).isTrue();
+
+    }
+
+    @Test
+    @DisplayName("실패")
+    public void f() throws Exception {
+
+      //when
+      teamService.addAnonymousBoard(teamId);
+
+      //then
+      assertThrows(RuntimeException.class, () -> teamService.addAnonymousBoard(teamId));
+    }
+
+  }
+
+  @Autowired
+  BoardRepository boardRepository;
+
+  @Test
+  @DisplayName("싱글톤 유지 확인 테스트")
+  public void anonymousBoardIsSingleTon() throws Exception {
+
+    //given
+    String teamName1 = "엑소";
+    Gender gender1 = Gender.MALE;
+    CreateTeamRequest request1 = CreateTeamRequest.builder()
+        .name(teamName1)
+        .gender(gender1)
+        .build();
+
+    Long teamId1 = teamService.save(request1);
+    teamService.addAnonymousBoard(teamId1);
+
+    //when
+    String teamName2 = "블랙핑크";
+    Gender gender2 = Gender.FEMALE;
+    CreateTeamRequest request2 = CreateTeamRequest.builder()
+        .name(teamName2)
+        .gender(gender2)
+        .build();
+
+    Long teamId2 = teamService.save(request2);
+    teamService.addAnonymousBoard(teamId2);
+
+    //then
+    Team team1 = teamRepository.findById(teamId1).orElseThrow(RuntimeException::new);
+    Team team2 = teamRepository.findById(teamId2).orElseThrow(RuntimeException::new);
+
+    Board board1 = team1.getBoardList().get(0);
+    Board board2 = team2.getBoardList().get(0);
+
+    assertThat(board1.getId()).isNotNull();
+    assertThat(board1).isEqualTo(board2);
 
   }
 
