6bedf11af40ee25840165cfaf73725802987e662
Hyunggeun447 <bopi447@naver.com>
2022-10-28 20:53:59 +0900

    feat: TeamRepository

---
+++ src/main/java/mission/mission/domain/team/repository/TeamRepository.java
@@ -0,0 +1,9 @@
+package mission.mission.domain.team.repository;
+
+import mission.mission.domain.team.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+
+}
