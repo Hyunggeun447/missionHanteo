0c4ce6d680a1e8a4d765ea8caf637d68c114f458
Hyunggeun447 <bopi447@naver.com>
2022-10-28 20:57:03 +0900

    feat: BoardConstant

---
+++ src/main/java/mission/mission/domain/board/constant/BoardConstant.java
@@ -0,0 +1,15 @@
+package mission.mission.domain.board.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BoardConstant {
+
+  public static final int NOTICE_BOARD_SEQ = 0;
+  public static final int ANONYMOUS_BOARD_SEQ = 1;
+  public static final int MEMBER_BOARD_SEQ = 2;
+
+  public static final String ANONYMOUS_BOARD_NAME = "익명게시판";
+
+}
