5938cd59bb12ba62713d2ee11517e5a001c896d8
Hyunggeun447 <bopi447@naver.com>
2022-10-28 20:58:04 +0900

    feat: Board 생성 수정 request dto

---
+++ src/main/java/mission/mission/domain/board/dto/request/CreateBoardRequest.java
@@ -0,0 +1,19 @@
+package mission.mission.domain.board.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreateBoardRequest {
+
+  private String name;
+
+  private Long teamId;
+
+}
---
+++ src/main/java/mission/mission/domain/board/dto/request/UpdateBoardRequest.java
@@ -0,0 +1,21 @@
+package mission.mission.domain.board.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class UpdateBoardRequest {
+
+  private Long id;
+
+  private String name;
+
+  private Long teamId;
+
+}
