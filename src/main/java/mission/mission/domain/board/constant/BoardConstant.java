f3a581cf79aeaf1b07ab031e34a42098cea917c3
Hyunggeun447 <bopi447@naver.com>
2022-10-29 19:49:08 +0900

    feat: seq 순서 변경

--- src/main/java/mission/mission/domain/board/constant/BoardConstant.java
+++ src/main/java/mission/mission/domain/board/constant/BoardConstant.java
@@ -7,8 +7,8 @@ import lombok.NoArgsConstructor;
 public class BoardConstant {
 
   public static final int NOTICE_BOARD_SEQ = 0;
-  public static final int ANONYMOUS_BOARD_SEQ = 1;
-  public static final int MEMBER_BOARD_SEQ = 2;
+  public static final int MEMBER_BOARD_SEQ = 1;
+  public static final int ANONYMOUS_BOARD_SEQ = 2;
 
   public static final String ANONYMOUS_BOARD_NAME = "익명게시판";
   public static final String NOTICE_BOARD_NAME = "공지사항";
