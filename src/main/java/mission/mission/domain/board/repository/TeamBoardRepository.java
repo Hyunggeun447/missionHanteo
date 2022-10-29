57a337a6555fe585c1ae69cc4c33cb8f4b293cc7
Hyunggeun447 <bopi447@naver.com>
2022-10-29 20:06:21 +0900

    clean:

--- src/main/java/mission/mission/domain/board/repository/TeamBoardRepository.java
+++
@@ -1,8 +0,0 @@
-package mission.mission.domain.board.repository;
-
-import mission.mission.domain.board.entity.TeamBoard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamBoardRepository extends JpaRepository<TeamBoard, Long> {
-
-}
