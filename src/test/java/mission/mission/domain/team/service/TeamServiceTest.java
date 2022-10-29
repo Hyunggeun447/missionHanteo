fd07fec2e048d36ef4301b019e7590e911fb04a7
Hyunggeun447 <bopi447@naver.com>
2022-10-29 20:55:34 +0900

    test:

--- src/test/java/mission/mission/domain/team/service/TeamServiceTest.java
+++ src/test/java/mission/mission/domain/team/service/TeamServiceTest.java
@@ -1,10 +1,17 @@
 package mission.mission.domain.team.service;
 
+import static mission.mission.domain.board.value.BoardType.MEMBER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.List;
+import mission.mission.domain.board.dto.request.CreateBoardRequest;
+import mission.mission.domain.board.dto.request.SearchRequest;
+import mission.mission.domain.board.dto.response.Response;
+import mission.mission.domain.board.dto.response.TeamResponse;
 import mission.mission.domain.board.entity.Board;
 import mission.mission.domain.board.repository.BoardRepository;
+import mission.mission.domain.board.service.BoardService;
 import mission.mission.domain.board.value.BoardType;
 import mission.mission.domain.team.dto.request.CreateTeamRequest;
 import mission.mission.domain.team.dto.request.UpdateTeamRequest;
@@ -32,6 +39,9 @@ class TeamServiceTest {
 
   @Autowired
   BoardRepository boardRepository;
+
+  @Autowired
+  BoardService boardService;
 
   @AfterEach
   void clean() {
@@ -131,8 +141,8 @@ class TeamServiceTest {
       teamId = teamService.save(request);
 
       team = teamRepository.findById(teamId).orElseThrow(RuntimeException::new);
-      board1 = boardRepository.save(BoardType.MEMBER.createBoard("name", team));
-      board2 = boardRepository.save(BoardType.MEMBER.createBoard("name", team));
+      board1 = boardRepository.save(MEMBER.createBoard("name", team));
+      board2 = boardRepository.save(MEMBER.createBoard("name", team));
     }
 
     @Test
@@ -232,6 +242,160 @@ class TeamServiceTest {
     assertThat(board1.getId()).isNotNull();
     assertThat(board1).isEqualTo(board2);
 
+  }
+
+  @Nested
+  @DisplayName("searchResult")
+  class searchResult {
+
+    Long teamId;
+    Long teamId2;
+
+    @BeforeEach
+    void setup() {
+      String teamName = "엑소";
+      Gender gender = Gender.MALE;
+      CreateTeamRequest request = CreateTeamRequest.builder()
+          .name(teamName)
+          .gender(gender)
+          .build();
+
+      teamId = teamService.save(request);
+
+      String teamName2 = "블랙핑크";
+      Gender gender2 = Gender.FEMALE;
+      CreateTeamRequest request2 = CreateTeamRequest.builder()
+          .name(teamName2)
+          .gender(gender2)
+          .build();
+
+      teamId2 = teamService.save(request2);
+
+      boardService.save(CreateBoardRequest.builder()
+          .name("백현")
+          .teamId(teamId)
+          .boardType(MEMBER)
+          .build());
+
+      boardService.save(CreateBoardRequest.builder()
+          .name("시우민")
+          .teamId(teamId)
+          .boardType(MEMBER)
+          .build());
+
+      boardService.save(CreateBoardRequest.builder()
+          .name("로제")
+          .teamId(teamId2)
+          .boardType(MEMBER)
+          .build());
+
+      boardService.save(CreateBoardRequest.builder()
+          .name("제니")
+          .teamId(teamId2)
+          .boardType(MEMBER)
+          .build());
+
+    }
+
+    @Test
+    @DisplayName("성공: ")
+    public void s() throws Exception {
+
+      //given
+      SearchRequest request = SearchRequest.builder()
+          .boardId(null)
+          .boardName(null)
+          .gender(null)
+          .teamName(null)
+          .build();
+
+      //when
+      Response response = teamService.searchResult(request);
+
+      //then
+
+      assertThat(response.getResult().containsKey(Gender.MALE)).isTrue();
+      assertThat(response.getResult().containsKey(Gender.FEMALE)).isTrue();
+
+      List<TeamResponse> teamResponses = response.getResult().get(Gender.MALE);
+      assertThat(teamResponses.get(0).getTeamName()).isEqualTo("엑소");
+
+      assertThat(teamResponses.get(0).getBoard().get(0).getBoardName()).isEqualTo("백현");
+
+    }
+
+    @Test
+    @DisplayName("성공: 성별 카테고리")
+    public void s_gender() throws Exception {
+
+      //given
+      SearchRequest request = SearchRequest.builder()
+          .boardId(null)
+          .boardName(null)
+          .gender(Gender.MALE)
+          .teamName(null)
+          .build();
+
+      //when
+      Response response = teamService.searchResult(request);
+
+      //then
+      assertThat(response.getResult().containsKey(Gender.MALE)).isTrue();
+      assertThat(response.getResult().containsKey(Gender.FEMALE)).isFalse();
+    }
+
+
+    @Test
+    @DisplayName("성공: teamName")
+    public void s_teamName() throws Exception {
+
+      //given
+      SearchRequest request = SearchRequest.builder()
+          .boardId(null)
+          .boardName(null)
+          .gender(null)
+          .teamName("블랙핑크")
+          .build();
+
+      //when
+      Response response = teamService.searchResult(request);
+
+      //then
+
+      assertThat(response.getResult().containsKey(Gender.MALE)).isFalse();
+      assertThat(response.getResult().containsKey(Gender.FEMALE)).isTrue();
+
+      List<TeamResponse> teamResponses = response.getResult().get(Gender.FEMALE);
+      assertThat(teamResponses.get(0).getTeamName()).isEqualTo("블랙핑크");
+
+    }
+
+    @Test
+    @DisplayName("성공: boardName")
+    public void s_boardName() throws Exception {
+
+      //given
+      SearchRequest request = SearchRequest.builder()
+          .boardId(null)
+          .boardName("제니")
+          .gender(null)
+          .teamName(null)
+          .build();
+
+      //when
+      Response response = teamService.searchResult(request);
+
+      //then
+
+      assertThat(response.getResult().containsKey(Gender.MALE)).isFalse();
+      assertThat(response.getResult().containsKey(Gender.FEMALE)).isTrue();
+
+      List<TeamResponse> teamResponses = response.getResult().get(Gender.FEMALE);
+      assertThat(teamResponses.get(0).getTeamName()).isEqualTo("블랙핑크");
+
+      assertThat(teamResponses.get(0).getBoard().get(1).getBoardName()).isEqualTo("제니");
+
+    }
   }
 
 }
